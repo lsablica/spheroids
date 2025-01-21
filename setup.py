fb4767d9b88bdd3e55040adae3815d36992ffa76
lukas <33362522+lsablica@users.noreply.github.com>
2025-01-21 13:28:13 +0100

    0.2.0 release

--- setup.py
+++ setup.py
@@ -65,7 +65,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 # Define the setup configuration
 setup(
     name="spheroids",
-    version="0.1.0",
+    version="0.2.0",
     author="Lukas Sablica",
     author_email="lsablica@wu.ac.at",
     description="A package for spherical clustering and probabilistic modeling",
